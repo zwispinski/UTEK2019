0ba60d49b71a48a0d6f9dcd82d849a1bece0f59d
Standard <Standard@Erics-MacBook-Air.local>
2019-01-12 11:00:13 -0500

    create readFile.py

---
+++ readFile.py
@@ -0,0 +1,12 @@
+# Get input from a textfile
+def readFile(file_name):
+    try:
+        f = open(file_name,'r')
+    except:
+        print "ERR: file", file_name, "is not present or can't be opened"
+
+    f = open(file_name,'r')
+    lines = f.readlines()
+    f.close()
+    
+    return [s[:-1] for s in lines]
