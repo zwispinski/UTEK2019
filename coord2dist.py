db5d02092522fffb8c33b88d5caf622d7b8cace9
Standard <Standard@Erics-MacBook-Air.local>
2019-01-12 14:25:36 -0500

    graph code

--- coord2dist.py
+++ coord2dist.py
@@ -1,10 +1,8 @@
 import numpy as np
+from distance import distance
 
 def coord2dist(C):
 
-    def distance(P, Q):
-        return ( (P[0] - Q[0])**2 + (P[1] - Q[1])**2 )**0.5
-    
     # C is a list of coordinates
     dist = np.zeros([len(C), len(C)])
     for i in range(len(C)):
---
+++ distance.py
@@ -0,0 +1,2 @@
+def distance(P, Q):
+        return ( (P[0] - Q[0])**2 + (P[1] - Q[1])**2 )**0.5
