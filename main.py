aef6a503adfde4f9ad100beddfbb6a2aa3e8aaad
ChristopherAlexiev <chris340a@gmail.com>
2019-01-12 13:49:19 -0500

    e

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #all the import statements
-import item.py
-import readFile.py
-import writeFile.py
+from item.py import item
+from readFile.py import readFile
+from writeFile.py import writeFile
 
 #read the file input and create a graph of items and robots
-
+print("hello")
 
 #partition the file input using the knapsack problem
 
 
 #implement the travelling salesman problem on each partition
-
+#input to the tsp is distance matrix
+#output of tps to be determined (string)
 
 #perform each timestep while recording the robots' movements; involves robots yielding to other robots
+given this list use dijkstra on the big graph
--- writeFile.py
+++ writeFile.py
@@ -3,7 +3,7 @@ def writeFile(file_name, line):
     try:
         g = open(file_name,'r')
     except:
-        print "ERR: file", file_name, "is not present or can't be opened"
+        print ("ERR: file", file_name, "is not present or can't be opened")
 
     g = open(file_name, 'w+')   # + means if the file does not exist, then python will create it
     g.write(line)
@@ -12,7 +12,7 @@ def writeFile(file_name, line):
 # If you want to replace a line of a textfile
 def replace_line(file_name, line_num, text):
     lines = open(file_name, 'r').readlines()
-    print lines[line_num] #line being replaced
+    #print (lines[line_num]) #line being replaced
 
     lines[line_num] = text
     out = open(file_name, 'w')
