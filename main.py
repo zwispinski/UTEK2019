50ec6666f7375899d120bfa5846d9880c2c427e5
ChristopherAlexiev <chris340a@gmail.com>
2019-01-12 13:23:00 -0500

    the start of main file

---
+++ main.py
@@ -0,0 +1,15 @@
+#all the import statements
+import item.py
+import readFile.py
+import writeFile.py
+
+#read the file input and create a graph of items and robots
+
+
+#partition the file input using the knapsack problem
+
+
+#implement the travelling salesman problem on each partition
+
+
+#perform each timestep while recording the robots' movements; involves robots yielding to other robots
