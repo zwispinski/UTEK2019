b2091a981fc84abafb4e35215c4d0a37b8322e1b
ChristopherAlexiev <chris340a@gmail.com>
2019-01-12 14:19:12 -0500

    j

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 #all the import statements
-from item.py import item
-from readFile.py import readFile
-from writeFile.py import writeFile
+from item import item
+from readFile import readFile
+from writeFile import writeFile
+#from knapsack import knapsack
+#from tsp import tsp
+#from dijkstra import diskstra
+#from distance import distance
+import numpy
+
+filename = "Competition Package/5a.in"
+
 
 #read the file input and create a graph of items and robots
-print("hello")
+lines = readFile(filename)
+
+lines[1] =  
 
 #partition the file input using the knapsack problem
+listOfObjectsInPartition = knapsack(list, location)
 
 
 #implement the travelling salesman problem on each partition
 #input to the tsp is distance matrix
 #output of tps to be determined (string)
+tsp()
 
 #perform each timestep while recording the robots' movements; involves robots yielding to other robots
-given this list use dijkstra on the big graph
+#given this list use dijkstra on the big graph
+dijkstra()
+
+
+
